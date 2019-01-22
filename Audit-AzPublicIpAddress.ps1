03ba1a6424460ede3b995a08b9f794bf33cbfeba
James Dumont <j.dumont@veebaze.com>
2019-01-22 14:52:51 +0100

    Publish a script that export public ip address

---
+++ Audit-AzPublicIpAddress.ps1
@@ -0,0 +1,140 @@
+﻿<#
+.SYNOPSIS
+  Get Azure Public Ip Address and extract the result in a csv file
+.DESCRIPTION
+  REQUIRED : Internet access & Already connected to an Azure tenant
+  REQUIRED : PowerShell modules
+    ModuleType Version    Name
+    ---------- -------    ----
+    Script     0.6.1      Az.Network
+    Script     0.6.1      Az.profile
+.PARAMETER LogFile
+   Optional
+   Log file path
+.NOTES
+   AUTHOR: James Dumont le Douarec
+.LINK
+    https://github.com/JamesDLD/AzureRm-PowerShell
+.EXAMPLE
+   .\Audit-AzPublicIpAddress.ps1
+#>
+
+param(
+  [Parameter(Mandatory=$false,HelpMessage='Log file path')]
+  [String]
+  $LogFile
+)
+
+################################################################################
+#                                 Function
+################################################################################
+#region function
+Function PipSummary_array{
+[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][String] $AzureSubscriptionName,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][String] $Name,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][String] $ResourceGroupName,
+		[Parameter(Mandatory=$true,ValueFromPipeline=$true)][String] $Location,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][String] $AssociatedObject
+	)
+	Process {
+        $private:tableObj=New-Object PSObject
+
+        $tableObj | Add-Member -Name AzureSubscriptionName -MemberType  NoteProperty -Value $AzureSubscriptionName
+        $tableObj | Add-Member -Name Name -MemberType NoteProperty -Value $Name
+        $tableObj | Add-Member -Name ResourceGroupName -MemberType NoteProperty -Value $ResourceGroupName
+        $tableObj | Add-Member -Name Location -MemberType NoteProperty -Value $Location
+        $tableObj | Add-Member -Name AssociatedObject -MemberType NoteProperty -Value $AssociatedObject
+
+        return $tableObj
+	}
+} 
+Function Generate_Log_Action([string]$Action, [ScriptBlock]$Command, [string]$LogFile){
+	$Output = "Info : $Action  ... "
+	Write-Host $Output -ForegroundColor Cyan
+    ((Get-Date -UFormat "[%d-%m-%Y %H:%M:%S]  : ") + "Info" + " : " + $Action) | Out-File -FilePath $LogFile -Append -Force
+	Try{
+		$Result = Invoke-Command -ScriptBlock $Command 
+	}
+	Catch {
+		$ErrorMessage = $_.Exception.Message
+		$Output = "On action $Action : $ErrorMessage"
+        ((Get-Date -UFormat "[%d-%m-%Y %H:%M:%S]  : ") + "Error" + " : " + $Output) | Out-File -FilePath $LogFile -Append -Force
+		Write-Error $Output
+		$Result = "Error"
+	}
+	Return $Result
+}
+#endregion
+
+################################################################################
+#                                 Variable
+################################################################################
+Set-StrictMode -Version 2
+$ErrorActionPreference = "Stop"
+$AzureRmSubscriptions = Get-AzSubscription 
+$PipSummary_array = @()
+$workfolder = Split-Path $script:MyInvocation.MyCommand.Path
+$date = Get-Date -UFormat "%d-%m-%Y"
+
+#If not provided, creating the log file
+if($LogFile -eq "")
+{
+    $LogPath = $workfolder + "\logs"
+    if(!(Test-Path $LogPath)){mkdir $LogPath}
+    $logFile = $LogPath + "\$date-" + $MyInvocation.MyCommand.Name + ".log"
+}
+
+$Action = "Importing the Module Az.Profile with MinimumVersion 0.6.1"
+$Command = {Import-Module Az.Profile -MinimumVersion 0.6.1 -ErrorAction Stop}
+$Result = Generate_Log_Action -Action $Action -Command $Command -LogFile $logFile
+if($Result -eq "Error"){Exit 1}
+
+$Action = "Importing the Module Az.Network with MinimumVersion 0.6.1"
+$Command = {Import-Module Az.Network -MinimumVersion 0.6.1 -ErrorAction Stop}
+$Result = Generate_Log_Action -Action $Action -Command $Command -LogFile $logFile
+if($Result -eq "Error"){Exit 1}
+#endregion
+
+################################################################################
+#                                 Action
+################################################################################
+foreach ($AzureRmSubscription in $AzureRmSubscriptions)
+{
+    $Action = "Getting the AzureRm context for the SubscriptionName : $($AzureRmSubscription.Name)"
+    $Command = {Get-AzSubscription -SubscriptionName $AzureRmSubscription.Name | Set-AzContext -ErrorAction Stop}
+    $AzureRmContext = Generate_Log_Action -Action $Action -Command $Command -LogFile $logFile
+    if($AzureRmContext -eq "Error"){Exit 1}
+
+    $Action = "Selecting the AzureRm SubscriptionName : $($AzureRmSubscription.Name)"
+    $Command = {Select-AzSubscription -Name $AzureRmSubscription.Name -Context $AzureRmContext -Force -ErrorAction Stop}
+    $Result = Generate_Log_Action -Action $Action -Command $Command -LogFile $logFile
+    if($Result -eq "Error"){Exit 1}
+
+    $Action = "Getting the Public Ip Address from the SubscriptionName : $($AzureRmSubscription.Name)"
+    $Command = {Get-AzPublicIpAddress -ErrorAction Stop}
+    $Pips = Generate_Log_Action -Action $Action -Command $Command -LogFile $logFile
+    if($Pips -eq "Error"){Exit 1}
+
+    foreach ($Pip in $Pips)
+    {
+        $AssociatedObject=""
+        if($Pip.IpConfiguration){$AssociatedObject=$Pip.IpConfiguration.Id.Split("/")[8]}
+        else{$AssociatedObject=0}
+
+        $PipSummary_array += PipSummary_array -AzureSubscriptionName $AzureRmSubscription.Name `
+            -Name $Pip.Name `
+            -ResourceGroupName $Pip.ResourceGroupName `
+            -Location $Pip.Location `
+            -AssociatedObject $AssociatedObject 
+    }
+}
+
+################################################################################
+#                                 Output
+################################################################################
+$Action = "Exporting the Pip summary audit result in to the file : $($workfolder + "\logs" + "\$date-PipSummary.csv")"
+$Command = {$PipSummary_array | export-csv $($workfolder + "\logs" + "\$date-PipSummary.csv") -notypeinformation -ErrorAction Stop}
+$Result = Generate_Log_Action -Action $Action -Command $Command -LogFile $logFile
+if($Result -eq "Error"){Exit 1}
