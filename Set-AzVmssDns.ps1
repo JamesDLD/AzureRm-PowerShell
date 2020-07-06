ba042e9d4e0e8433205fc100a7a3b7d590962e69
James Dumont <j.dumont@veebaze.com>
2020-07-06 17:47:01 +0200

    Restart vm scale set

--- Set-AzVmssDns.ps1
+++ Set-AzVmssDns.ps1
@@ -1,7 +1,7 @@
 ## Global variables
 $AzureRmSubscriptionName = "mvp-sub1"
-$RgName = "myRgNamee"
-$VMScaleSetName = "aks-nodepool1-vmss"
+$RgName = "train-jdld-dev1-rg1"
+$VMScaleSetName = "demo-vmss1"
 
 ## Connectivity
 # Login first with Connect-AzAccount if not using Cloud Shell
@@ -12,17 +12,19 @@ Select-AzSubscription -Name $AzureRmSubscriptionName -Context $AzureRmContext -F
 $Vmss = Get-AzVmss -ResourceGroupName $RgName -VMScaleSetName $VMScaleSetName
 
 # Add DNS servers on the IP configuration
-$Vmss.VirtualMachineProfile.NetworkProfile.networkInterfaceConfigurations.DnsSettings.DnsServers.Add("192.168.2.5")
-$Vmss.VirtualMachineProfile.NetworkProfile.networkInterfaceConfigurations.DnsSettings.DnsServers.Add("192.168.2.6")
+$Vmss.VirtualMachineProfile.NetworkProfile.networkInterfaceConfigurations.DnsSettings.DnsServers.Add("10.0.1.4")
+$Vmss.VirtualMachineProfile.NetworkProfile.networkInterfaceConfigurations.DnsSettings.DnsServers.Add("10.0.1.5")
 
 # Update the model of the scale set with the new configuration in the local PowerShell object
 Update-AzVmss -ResourceGroupName $RgName -VMScaleSetName $VMScaleSetName -virtualMachineScaleSet $vmss
 
-# Bring VMs up-to-date with the latest scale set model
+# Bring VMs up-to-date with the latest scale set model and Restart it
 $AzVmssVMs = Get-AzVmssVM -ResourceGroupName $RgName -VMScaleSetName $VMScaleSetName
 foreach ($AzVmssVM in $AzVmssVMs) {
-    $reply = Read-Host -Prompt "This action will manually upgrade $($AzVmssVM.Name) to the latest model. While upgrading, the instance may be restarted. Do you want to upgrade $($AzVmssVM.Name)? Continue?[y/n]"
+    $reply = Read-Host -Prompt "This action will manually upgrade $($AzVmssVM.Name) to the latest model. While upgrading, the instance WILL be restarted. Do you want to upgrade $($AzVmssVM.Name)? Continue?[y/n]"
     if ( $reply -match "[yY]" ) { 
         Update-AzVmssInstance -ResourceGroupName $RgName -VMScaleSetName $VMScaleSetName -InstanceId $AzVmssVM.InstanceId
+
+        Restart-AzVmss -ResourceGroupName $RgName -VMScaleSetName $VMScaleSetName -InstanceId $AzVmssVM.InstanceId
     }
 }  
